4f9608d77c60740b1bcd1fad7c504f29f5282f98
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-07-17 15:26:13 +0100

    add implementation for wrapped arg marshaller

--- src/Aidan.SerialRPC.Marshalling/WrappedArgMarshalling/WrappedArgMarshaller.cs
+++ src/Aidan.SerialRPC.Marshalling/WrappedArgMarshalling/WrappedArgMarshaller.cs
@@ -4,5 +4,13 @@ namespace Aidan.SerialRPC.Marshalling.WrappedArgMarshalling;
 
 public class WrappedArgMarshaller : IWrappedArgMarshaller
 {
-    public byte [ ] Marshal( Func<byte [ ]> dataIn ) { return Array.Empty<byte>( ); }
+    public byte [ ] Marshal( Func<byte [ ]> dataIn )
+    {
+        var bytes = dataIn.Invoke( );
+        var bytesWithStartAndEnd = new List<byte>( );
+        bytesWithStartAndEnd.Add( 0xFF );
+        bytesWithStartAndEnd.AddRange( bytes );
+        bytesWithStartAndEnd.Add( 0x00 );
+        return bytesWithStartAndEnd.ToArray( );
+    }
 }
